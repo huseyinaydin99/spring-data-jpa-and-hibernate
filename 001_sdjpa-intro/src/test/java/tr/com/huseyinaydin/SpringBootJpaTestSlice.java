977de81050e6ac8ca45d879dcbb119869ae89224
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-12-27 15:42:57 +0300

    Test sırası ve işlem yönetimi(transaction) için dipnotlar eklendi.
    Testlerin sırasını belirlemek için @TestMethodOrder(MethodOrderer.OrderAnnotation.class) dipnotunu kullandım, böylece testlerin belirli bir sırayla çalışmasını sağladım. İlk testte @Order(1) ile kitap ekleme işlemini gerçekleştirdim ve kitap sayısının arttığını doğruladım. İkinci testte ise @Order(2) ile veritabanındaki kitap sayısını kontrol ettim, burada işlem başarısız olsa bile yapılan değişikliklerin veritabanına yansıması için @Rollback(value = false) kullandım. @Commit dipnotu ile ilk testin başarılı olsun olmasın veritabanına kaydedilmesini sağladım. Bu yapı, testlerin sırasını korurken işlem yönetimi ve veritabanı değişikliklerini de kontrol etmeme olanak tanıdı.

--- 001_sdjpa-intro/src/test/java/tr/com/huseyinaydin/SpringBootJpaTestSlice.java
+++ 001_sdjpa-intro/src/test/java/tr/com/huseyinaydin/SpringBootJpaTestSlice.java
@@ -1,26 +1,43 @@
 package tr.com.huseyinaydin;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
 import tr.com.huseyinaydin.domain.Book;
 import tr.com.huseyinaydin.repositories.BookRepository;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
 public class SpringBootJpaTestSlice {
     @Autowired
     BookRepository bookRepository;
 
+    @Commit //test başarılı olsun olmasın commit edilir.
+    @Order(1)
     @Test
     void testJpaTestSplice(){
         long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(0);
 
         bookRepository.save(new Book("Kitap 1", "1994", "Hüseyin AYDIN"));
 
         long countAfter = bookRepository.count();
 
         assertThat(countBefore).isLessThan(countAfter);
+    }
+
+    @Rollback(value = false)//test başarısız olduğunda rollback yapılır.
+    @Order(2)
+    @Test
+    void testJpaTestSpliceTransaction(){
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(1);
     }
 }
