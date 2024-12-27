52a30201a53e7b71725b3fc927a827b318ac5881
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-12-27 14:20:29 +0300

    BookRepository testi.
    Spring Boot test yapısını kullanarak BookRepository'yi test etmeye başladım. count() metodu ile veritabanındaki kitap sayısını kontrol ettim ve en az bir kitap bulunduğunu doğruladım. Ayrıca, uygulamanın düzgün şekilde yüklenip yüklenmediğini kontrol etmek için contextLoads testini ekledim. Bu sayede, repository'nin doğru çalıştığını ve uygulamanın düzgün başlatıldığını test ettim. Bu satır(assertThat(count).isGreaterThan(0);), veritabanında en az bir kitap bulunduğunu doğrulamak için count() metodunun sonucunun 0'dan büyük olmasını bekler. 0'dan büyükse test geçer değilse kalır.

--- 001_sdjpa-intro/src/test/java/tr/com/huseyinaydin/ApplicationTests.java
+++ 001_sdjpa-intro/src/test/java/tr/com/huseyinaydin/ApplicationTests.java
@@ -1,13 +1,26 @@
 package tr.com.huseyinaydin;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import tr.com.huseyinaydin.repositories.BookRepository;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class ApplicationTests {
 
+	@Autowired
+	BookRepository bookRepository;
+
+	@Test
+	void testBookRepository(){
+		long count = bookRepository.count();
+
+		assertThat(count).isGreaterThan(0);
+	}
+
 	@Test
 	void contextLoads() {
 	}
-
 }
