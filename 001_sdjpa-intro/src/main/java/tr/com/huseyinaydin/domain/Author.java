d04e92e8d0f063187c656ecc901bf0f4ec00e074
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-12-31 12:19:27 +0300

    auto_increment ve identity stratejisiyle birincil anahtar yönetimi.
    alter table books change id id BIGINT auto_increment;
    Books tablosunda id sütununu auto_increment olacak şekilde güncelledim, birincil anahtar değerlerini otomatik oluşturmayı sağladım.
    
    alter table authors change id id BIGINT auto_increment;
    Authors tablosunda id sütununu auto_increment olarak düzenledim, Hibernate ile uyumlu hale getirdim.
    
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    Entity sınıflarında birincil anahtarların otomatik oluşturulması için @GeneratedValue(strategy = GenerationType.IDENTITY) anotasyonunu kullandım.

--- 001_sdjpa-intro/src/main/java/tr/com/huseyinaydin/domain/Author.java
+++ 001_sdjpa-intro/src/main/java/tr/com/huseyinaydin/domain/Author.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Table(name = "authors")
 public class Author {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String firstName;
---
+++ 001_sdjpa-intro/src/main/resources/db/migration/V4__autoincrement_pk.sql
@@ -0,0 +1,2 @@
+alter table books change id id BIGINT auto_increment;
+alter table authors change id id BIGINT auto_increment;
