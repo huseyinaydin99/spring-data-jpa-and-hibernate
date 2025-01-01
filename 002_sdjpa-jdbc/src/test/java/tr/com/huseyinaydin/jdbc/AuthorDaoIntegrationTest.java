363d4e61f42f5bcbe8742ec55f3c789522ec1aee
huseyinaydin99 <huseyinaydin99@gmail.com>
2025-01-01 16:32:15 +0300

    Book entity için TDD yaklaşımıyla CRUD işlemleri için entegrasyon testleri yazıldı.
    Book varlık sınıfı(entity) için gerekli olan işlemleri test etmeyi hedefledim. İlk olarak, testDeleteBook metodunda bir kitap nesnesi oluşturup, bunu veritabanına kaydettim ve ardından kaydettiğim kitabı sildim. Kitabın silindiğini doğrulamak için veritabanından tekrar çekip null döndüğünü kontrol ettim.
    
    Sonrasında, updateBookTest metodunda kaydedilmiş bir kitabın başlığını güncelleyerek, güncellenen kitabın veritabanında doğru şekilde yansıdığını doğruladım. testSaveBook metoduyla ise yeni bir kitabı kaydettim ve kaydın başarılı olduğunu kontrol ettim.
    
    Ayrıca, testGetBookByName metodunda bir kitabı ismiyle arayarak doğru kitabı bulduğumdan emin oldum. Son olarak, testGetBook metodunda ise belirli bir ID'ye sahip kitabı veritabanından alıp, kitabın gerçekten var olduğunu doğruladım.
    
    Bunların hepsini yaptım ve testlerimin doğru çalıştığını kontrol ettim.

--- 002_sdjpa-jdbc/src/test/java/tr/com/huseyinaydin/jdbc/AuthorDaoIntegrationTest.java
+++ 002_sdjpa-jdbc/src/test/java/tr/com/huseyinaydin/jdbc/AuthorDaoIntegrationTest.java
@@ -2,6 +2,7 @@ package tr.com.huseyinaydin.jdbc;
 
 import tr.com.huseyinaydin.jdbc.dao.AuthorDao;
 import tr.com.huseyinaydin.jdbc.dao.AuthorDaoImpl;
+import tr.com.huseyinaydin.jdbc.dao.BookDao;
 import tr.com.huseyinaydin.jdbc.domain.Author;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import tr.com.huseyinaydin.jdbc.domain.Book;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -20,6 +22,75 @@ public class AuthorDaoIntegrationTest {
 
     @Autowired
     AuthorDao authorDao;
+
+    @Autowired
+    BookDao bookDao;
+
+    @Test
+    void testDeleteBook() {
+        Book book = new Book();
+        book.setIsbn("1234");
+        book.setPublisher("Hüseyin");
+        book.setTitle("Canım Kitabım(uydurdum :D)");
+        Book saved = bookDao.saveNewBook(book);
+
+        bookDao.deleteBookById(saved.getId());
+
+        Book deleted = bookDao.getById(saved.getId());
+
+        assertThat(deleted).isNull();
+    }
+
+    @Test
+    void updateBookTest() {
+        Book book = new Book();
+        book.setIsbn("1234");
+        book.setPublisher("Hüseyin");
+        book.setTitle("Canım Kitabım(uydurdum :D)");
+
+        Author author = new Author();
+        author.setId(3L);
+
+        book.setAuthor(author);
+        Book saved = bookDao.saveNewBook(book);
+
+        saved.setTitle("Yep Yeni Kitap");
+        bookDao.updateBook(saved);
+
+        Book fetched = bookDao.getById(saved.getId());
+
+        assertThat(fetched.getTitle()).isEqualTo("Yep Yeni Kitap");
+    }
+
+    @Test
+    void testSaveBook() {
+        Book book = new Book();
+        book.setIsbn("1234");
+        book.setPublisher("Hüseyin");
+        book.setTitle("Canım Kitabım(uydurdum :D)");
+
+        Author author = new Author();
+        author.setId(3L);
+
+        book.setAuthor(author);
+        Book saved = bookDao.saveNewBook(book);
+
+        assertThat(saved).isNotNull();
+    }
+
+    @Test
+    void testGetBookByName() {
+        Book book = bookDao.findBookByTitle("Temiz Temiz Kod Kitabı");
+
+        assertThat(book).isNotNull();
+    }
+
+    @Test
+    void testGetBook() {
+        Book book = bookDao.getById(3L);
+
+        assertThat(book.getId()).isNotNull();
+    }
 
     @Test
     void testSaveAuthor() {
