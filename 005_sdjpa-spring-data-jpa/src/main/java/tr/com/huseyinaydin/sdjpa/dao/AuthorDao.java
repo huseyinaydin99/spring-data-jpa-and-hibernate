38c4c824559dc5964ae8968b360d9212f70b9a7d
huseyinaydin99 <huseyinaydin99@gmail.com>
2025-01-10 15:11:31 +0300

    Hibernate ile Book DAO sınıfı oluşturuldu ve CRUD işlemleri eklendi.
    AuthorDaoHibernate sınıfını yazdım ve burada EntityManagerFactory kullanarak JPA ile veri tabanı işlemleri gerçekleştirecek bir yapı kurdum. Özellikle findAllAuthorsByLastName metodunda, soyadına göre yazarları listelemek için Hibernate kullanarak dinamik bir sorgu oluşturdum. Ayrıca, Pageable özelliğini de entegre ettim, böylece veriyi sayfalama yaparak alabiliyorum. Burada, sıralama kriterlerini kontrol edip, kullanıcıya uygun bir şekilde sıralama yapacak sorgu yazdım. Bu sınıf, EntityManager'ı yöneterek, veri tabanı işlemleri için temel bir yapı sağlıyor. Diğer metodları henüz tamamlamadım, ancak temel işlevsellik için gerekli olan yapıyı kurdum.
    
    BookDaoHibernate sınıfını yazdım ve burada JPA kullanarak temel CRUD işlemlerini gerçekleştirecek yapıyı kurdum. EntityManagerFactory ile EntityManager oluşturup, veri tabanına erişim sağladım. Örneğin, findAllBooks metodunda, kitapları sayfalayarak sorgulamak için Pageable parametresiyle dinamik bir query oluşturdum. Ayrıca, tüm kitapları listeleyen findAllBooks() metodunda da veri tabanındaki kitapları direkt olarak çekmek için benzer bir yaklaşım kullandım. saveNewBook ve updateBook metodlarında ise, kitapları veri tabanına kaydedip güncellemek için Transaction yönetimi sağladım. getById metodunda ise, belirli bir id’ye sahip kitabı bulup geri döndürüyorum. findBookByTitle metodunda ise başlığa göre kitap araması yaparak, native SQL sorgusu ile tek bir kitap getiriyorum. CRUD işlemlerinin tamamını EntityManager üzerinde gerçekleştirip, işlemler sonrasında bağlantıyı kapatıyorum. Bu şekilde, Hibernate ile veri tabanı üzerinde temel işlemleri verimli bir şekilde yönetiyorum.
    
    Hibernate, Java'da veritabanı işlemleri için kullanılan, nesne yönelimli bir framework'tür ve JPA (Java Persistence API) ile uyumlu çalışarak veritabanı işlemlerini yönetir. JPA ise Java'da nesne veritabanı eşlemesi (ORM) sağlayan bir API'dir ve veritabanı ile nesneler arasında veri aktarımını kolaylaştırır.
    ORM (Object-Relational Mapping), nesne tabanlı programlama dillerindeki nesneler ile ilişkisel veritabanlarındaki tablolar arasında veri dönüşümünü sağlayan bir tekniktir.

--- 005_sdjpa-spring-data-jpa/src/main/java/tr/com/huseyinaydin/sdjpa/dao/AuthorDao.java
+++ 005_sdjpa-spring-data-jpa/src/main/java/tr/com/huseyinaydin/sdjpa/dao/AuthorDao.java
@@ -1,8 +1,14 @@
 package tr.com.huseyinaydin.sdjpa.dao;
 
+import org.springframework.data.domain.Pageable;
 import tr.com.huseyinaydin.sdjpa.domain.Author;
 
+import java.util.List;
+
 public interface AuthorDao {
+
+    List<Author> findAllAuthorsByLastName(String lastname, Pageable pageable);
+
     Author getById(Long id);
 
     Author findAuthorByName(String firstName, String lastName);
