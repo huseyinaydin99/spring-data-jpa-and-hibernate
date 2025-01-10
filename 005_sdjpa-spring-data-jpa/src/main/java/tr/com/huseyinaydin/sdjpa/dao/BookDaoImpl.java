23f27a2ac01db795aeafe5014dd6f341b9f2a0f4
huseyinaydin99 <huseyinaydin99@gmail.com>
2025-01-10 17:06:47 +0300

    Spring Data JPA findAllBooksSortByTitle metodu eklendi ve kitaplar sıralı şekilde döndürüldü.
    BookDaoImpl sınıfına, başlıklarına göre sıralı şekilde kitapları getiren findAllBooksSortByTitle metodunu ekledim. Bu metod, sayfalama parametrelerini alıp kitapları sıralayarak döndürüyor.

--- 005_sdjpa-spring-data-jpa/src/main/java/tr/com/huseyinaydin/sdjpa/dao/BookDaoImpl.java
+++ 005_sdjpa-spring-data-jpa/src/main/java/tr/com/huseyinaydin/sdjpa/dao/BookDaoImpl.java
@@ -2,6 +2,7 @@ package tr.com.huseyinaydin.sdjpa.dao;
 
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
@@ -21,7 +22,9 @@ public class BookDaoImpl implements BookDao {
 
     @Override
     public List<Book> findAllBooksSortByTitle(Pageable pageable) {
-        return null;
+        Page<Book> bookPage = bookRepository.findAll(pageable);
+
+        return bookPage.getContent();
     }
 
     @Override
