aab3af94d20150ece83a621e593cd9b8c39501d3
huseyinaydin99 <huseyinaydin99@gmail.com>
2025-02-05 13:53:41 +0300

    ⚠️ LazyInitializationException ve Veritabanı İlişkileri 🔄
    🚨 LazyInitializationException hatası ile karşılaşıldı, çünkü orderHeader.getOrderLines() ile ilişkili veriler lazily (tembel) yüklendi.
    🔄 @Transactional annotation'ı eklendi, böylece ilişkili veriler transaction kapsamında yüklenip kullanılabilir hale geldi.
    🛠️ Lazy loading sorunu çözülerek, ilişkili Product ve Category nesneleri doğru şekilde erişildi ve yazdırıldı.
    💡 Lazy yükleme ve Eager yükleme arasındaki fark göz önünde bulundurularak, işlem akışı düzeltildi.
    ✅ Veri erişimi ve transaction yönetimi düzgün şekilde sağlandı.

--- 006_sdjpa-order-service/src/main/java/tr/com/huseyinaydin/orderservice/bootstrap/Bootstrap.java
+++ 006_sdjpa-order-service/src/main/java/tr/com/huseyinaydin/orderservice/bootstrap/Bootstrap.java
@@ -1,13 +1,29 @@
 package tr.com.huseyinaydin.orderservice.bootstrap;
 
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import tr.com.huseyinaydin.orderservice.domain.OrderHeader;
+import tr.com.huseyinaydin.orderservice.repositories.OrderHeaderRepository;
 
 @Component
 public class Bootstrap implements CommandLineRunner {
 
+    @Autowired
+    OrderHeaderRepository orderHeaderRepository;
+
+    @Transactional
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("run metodu çalıştırıldı!");
+        OrderHeader orderHeader = orderHeaderRepository.findById(1L).get();
+
+        orderHeader.getOrderLines().forEach(ol -> {
+            System.out.println(ol.getProduct().getDescription());
+
+            ol.getProduct().getCategories().forEach(cat -> {
+                System.out.println(cat.getDescription());
+            });
+        });
     }
 }
