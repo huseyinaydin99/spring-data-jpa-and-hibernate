ae11a31599a9bccb1aae1c9485589a7967123eca
huseyinaydin99 <huseyinaydin99@gmail.com>
2025-02-09 15:39:25 +0300

    🎉 Legacy Database Mapping Projesi Başlatıldı! 🚀
    Projeye başladım! 🛠️ pom.xml dosyasını yapılandırarak gerekli bağımlılıkları ekledim. Spring Boot, JPA ve Flyway'i entegre ettim. 🔧 MySQL veritabanını kurdum ve yapılandırdım. 🗄️
    
    Flyway ile veritabanı migrasyonlarını yönetebilmek için flyway-core bağımlılığını ekledim. Ayrıca, MySQL bağlantısı için gerekli ayarları yaptım. Veritabanı kullanıcılarını oluşturup yetkileri ayarladım, wpadmin ve wpuser kullanıcılarına doğru yetkileri verdim. 💻
    
    Veritabanı bağlantısı için application.properties dosyasına gerekli tüm ayarları ekledim ve şema migration'larını Flyway'e yönlendirdim. 🎯
    
    Bu proje bana çok şey katacak. 💡 Şimdi bir sonraki adım için heyecanlıyım! 😎
    
    wpadmin ve wpuser veritabanı kullanıcılarını oluşturup, doğru yetkilerle yapılandırdım. 🛡️ Bu kullanıcılar ile veritabanı erişimlerini düzgün şekilde sağlıyorum. Her şey güvenli! 🔒
    
    Veritabanı üzerinde yapılan işlemleri doğru şekilde izlemek ve yönetmek için her iki kullanıcıya da ilgili yetkileri verdim. Bu, sistemin güvenli ve verimli çalışmasını sağlayacak. ✅

---
+++ 008_sdjpa-wp/src/main/java/tr/com/huseyinaydin/SdjpaJpaApplication.java
@@ -0,0 +1,12 @@
+package tr.com.huseyinaydin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdjpaJpaApplication.class, args);
+    }
+}
