9da33e6a85343d4d29c06c75cc2bbff5234fe7b3
huseyinaydin99 <huseyinaydin99@gmail.com>
2025-02-09 14:39:35 +0300

    🛠️ JPA'de Mapped Superclass Olayına Girdim! 🚀💡
    🏗️ BaseEntity diye bir süper sınıf koydum, ID olayını buraya taşıdım, her defasında tekrar yazmaktan kurtuldum! 🙌✨
    🔄 OrderHeader artık BaseEntity’den miras alıyor, gereksiz kod yazmaktan kurtuldum, mis gibi oldu! 🍃😎
    🏛️ @MappedSuperclass sayesinde tablo oluşturmadı, ama mirası kaptı, tam istediğim gibi! 🤓🔥
    ⏩ Kodlar daha temiz, tekrar yok, düzen ve sürdürülebilirlik var! Keyifler gıcır, devamke! 🚀😃

---
+++ 007_sdjpa-inheritance/src/main/java/tr/com/huseyinaydin/SdjpaInheritenceApplication.java
@@ -0,0 +1,12 @@
+package tr.com.huseyinaydin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaInheritenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdjpaInheritenceApplication.class, args);
+    }
+}
---
+++ 007_sdjpa-inheritance/src/main/java/tr/com/huseyinaydin/domain/msuper/OrderHeader.java
@@ -0,0 +1,17 @@
+package tr.com.huseyinaydin.domain.msuper;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class OrderHeader extends BaseEntity {
+
+    private String customerName;
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+}
