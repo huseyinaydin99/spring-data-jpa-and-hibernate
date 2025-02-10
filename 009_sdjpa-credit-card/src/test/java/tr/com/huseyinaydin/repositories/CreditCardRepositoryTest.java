53d992e2c51611edc6143c52048ba8d8e38cdefb
huseyinaydin99 <huseyinaydin99@gmail.com>
2025-02-10 15:03:50 +0300

    🚀 Hibernate Interceptors Projesi Başlatıldı ve MySQL Veritabanı Yapılandırıldı 🛠️
    Projeye Hibernate Interceptors desteği ekledim ve MySQL veritabanı için gerekli yapılandırmaları tamamladım. İlk olarak, mysql-setup.sql dosyasında, payment adlı veritabanını oluşturup, iki kullanıcı (paymentadmin ve paymentuser) tanımladım ve gerekli yetkileri verdim 🔐. Ardından, Hibernate ile entegre olacak CreditCard adlı bir entity sınıfı oluşturup, bu sınıfın veritabanındaki credit_card tablosuyla eşleşmesini sağladım 🏷️. V1__create_db.sql dosyasını oluşturarak, credit_card tablosunun yapısını belirledim 📝. Ayrıca, application.properties dosyasını düzenleyerek Spring Boot'un veritabanına bağlanmasını ve Flyway ile veritabanı migrasyonlarını yönetmesini sağladım ⚙️. Bu adımlarla projede veritabanı bağlantısı ve Hibernate yapılandırması tamamlandı! 😎🎉

---
+++ 009_sdjpa-credit-card/src/test/java/tr/com/huseyinaydin/repositories/CreditCardRepositoryTest.java
@@ -0,0 +1,37 @@
+package tr.com.huseyinaydin.repositories;
+
+import tr.com.huseyinaydin.domain.CreditCard;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("local")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CreditCardRepositoryTest {
+
+    final String CREDIT_CARD = "12345678900000";
+
+    @Autowired
+    CreditCardRepository creditCardRepository;
+
+    @Test
+    void testSaveAndStoreCreditCard() {
+        CreditCard creditCard = new CreditCard();
+        creditCard.setCreditCardNumber(CREDIT_CARD);
+        creditCard.setCvv("177");
+        creditCard.setExpirationDate("12/2028");
+
+        CreditCard savedCC = creditCardRepository.saveAndFlush(creditCard);
+
+        System.out.println("Kredi kartı database'den okunuyor");
+
+        CreditCard fetchedCC = creditCardRepository.findById(savedCC.getId()).get();
+
+        assertThat(savedCC.getCreditCardNumber()).isEqualTo(fetchedCC.getCreditCardNumber());
+    }
+}
